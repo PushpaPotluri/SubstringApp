1c791196f69efbf8a7681b254b4b2822646b202e
Pushpa Sree Potluri <Pushpa Sree Potluri>
2016-08-24 14:00:50 -0400

    first commit

---
+++ 10SubstringApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10SubstringApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "20000";
+            long[] l10SubstringsArray = GetAll10Substrings(number).Result;
+            
+            foreach(var n in l10SubstringsArray)
+                Console.WriteLine(n);
+
+            Console.Read();
+        }
+
+        public static async Task<long[]> GetAll10Substrings(string number)
+        {
+            List<long> i10SubstringArray = new List<long>();
+            long no;
+            
+            if (!long.TryParse(number, out no))
+                return i10SubstringArray.ToArray();
+
+            Parallel.For(0, no, n =>
+            {
+                if (Is10Substring(n.ToString()).Result)
+                    i10SubstringArray.Add(n);
+            });
+
+            return i10SubstringArray.ToArray();
+        }
+
+        public static async Task<bool> Is10Substring(string no)
+        {
+            bool b10Substring = false;
+            int i;
+            for (i = 0; i < no.Length; i++)
+            {
+                int sum = 0;
+                int j;
+                b10Substring = false;
+                for (j = i; j < no.Length; j++)
+                {
+                    int digit = 0;
+                    int.TryParse(no[j].ToString(), out digit);
+                    sum += digit;
+                    if (sum > 10)
+                    {
+                        b10Substring = b10Substring == true;
+                        break;
+                    }
+
+                    b10Substring = (sum == 10);
+                }
+
+                if (j == no.Length || b10Substring == false)
+                {
+                    break;
+                }
+            }
+
+            return b10Substring;
+        }
+    }
+}
---
+++ 10SubstringAppTests/ProgramTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _10SubstringApp;
+
+namespace _10SubstringAppTests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void GetAll10SubstringsSuccess()
+        {
+            // Arrange
+            string number = "100";
+
+            // Act
+            var list10Substring = Program.GetAll10Substrings(number).Result;
+
+            // Assert
+            Assert.IsTrue(list10Substring.Any());
+        }
+
+        [TestMethod]
+        public void GetAll10SubstringsZeroNumberSuccess()
+        {
+            // Arrange
+            string number = "0";
+
+            // Act
+            var list10Substring = Program.GetAll10Substrings(number).Result;
+
+            // Assert
+            Assert.IsFalse(list10Substring.Any());
+        }
+
+        [TestMethod]
+        public void GetAll10SubstringsInvalidNumberFail()
+        {
+            // Arrange
+            string number = "ACD42";
+
+            // Act
+            var list10Substring = Program.GetAll10Substrings(number).Result;
+
+            // Assert
+            Assert.IsFalse(list10Substring.Any());
+        }
+
+        [TestMethod]
+        public void Is10SubstringsSuccess()
+        {
+            // Arrange
+            string number = "3523014";
+
+            // Act
+            var result = Program.Is10Substring(number).Result;
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Is10SubstringsFail()
+        {
+            // Arrange
+            string number = "28546";
+
+            // Act
+            var result = Program.Is10Substring(number).Result;
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
